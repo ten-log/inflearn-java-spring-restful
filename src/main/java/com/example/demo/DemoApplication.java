a0ed648fbb3c57566fecba36da0ce9b90095882a
ten-log <qlqjs674@naver.com>
2023-12-10 11:00:31 +0900

    Test: What kind of items do you have in iocContainer

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,18 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        ApplicationContext ac = SpringApplication.run(DemoApplication.class, args);
+
+        String[] allBeanNames = ac.getBeanDefinitionNames();
+        for (String beanName : allBeanNames) {
+            System.out.println(beanName);
+        }
     }
 
 }
