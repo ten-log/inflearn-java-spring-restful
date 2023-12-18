791c497e2cf4ba8d8cf9964d922aa8d787e3961f
ten-log <qlqjs674@naver.com>
2023-12-18 15:32:36 +0900

    Add: create save file api

--- src/main/java/com/example/demo/controller/HelloWorldController.java
+++ src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -1,14 +1,16 @@
 package com.example.demo.controller;
 
 import com.example.demo.bean.HelloWorldBean;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.context.MessageSource;
+import org.springframework.ui.Model;
 import org.springframework.web.ErrorResponseException;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Locale;
 
 
@@ -23,9 +25,27 @@ public class HelloWorldController {
         this.messageSource = messageSource;
     }
 
-    //GET
-    //
-//    @RequestMapping(method = RequestMethod.GET, path = "/asdf")
+
+    @PostMapping("/upload")
+    public String fileUpload(@RequestParam("uploadFile") MultipartFile file, Model model
+            , HttpServletRequest request
+    ) throws IOException {
+        String savedFileName = "test789";
+        // 1. 파일 저장 경로 설정 : 실제 서비스되는 위치(프로젝트 외부에 저장)
+        String uploadPath = "C:/Users/TEN/Downloads/";
+        // 2. 원본 파일 이름 알아오기
+        String originalFileName = "test456";
+        // 3. 파일 이름 중복되지 않게 이름 변경(서버에 저장할 이름) UUID 사용
+        String fileName = "test123.mp3";
+        // 4. 파일 생성
+        File file1 = new File(uploadPath + fileName);
+        // 5. 서버로 전송
+        file.transferTo(file1);
+        // model로 저장
+        model.addAttribute("originalFileName", originalFileName);
+        return "upload/fileUploadResult";
+    }
+
     @GetMapping(path = "/hello-world")
     public String helloWorld() {
         return "test";
